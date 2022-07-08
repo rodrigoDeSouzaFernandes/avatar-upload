9650d9756b31e56e2c8de48e7032db454796b99f
rodrigo <rdesouza2008@gmail.com>
2022-07-08 20:25:54 -0300

    implementa lib dropzone e inicia estilização do componente

--- src/Components/AvatarUpload/avatarUpload.tsx
+++ src/Components/AvatarUpload/avatarUpload.tsx
@@ -1,5 +1,34 @@
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+
 function AvatarUpload() {
-  return <div>oi</div>;
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    console.log(acceptedFiles);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive, isDragReject } =
+    useDropzone({ onDrop, accept: { "image/*": [] } });
+
+  const dropzoneText = () => {
+    if (isDragReject) {
+      return <p>Arquivo não suportado</p>;
+    }
+    if (isDragActive) {
+      return <p>Solte o arquivo aqui</p>;
+    }
+    return <p>Arraste e solte sua imagem aqui, ou clique para selecioná-la</p>;
+  };
+
+  return (
+    <section className="container">
+      <div className="avatarUpload">
+        <label {...getRootProps()} htmlFor="click">
+          <input {...getInputProps()} id="click" />
+          {dropzoneText()}
+        </label>
+      </div>
+    </section>
+  );
 }
 
 export default AvatarUpload;
